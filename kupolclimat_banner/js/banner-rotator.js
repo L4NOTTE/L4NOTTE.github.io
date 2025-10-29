1fb7a3ba762ec0533ea1bd4d6d406f14c50dfb0f
L4NOTTE <145873589+L4NOTTE@users.noreply.github.com>
2025-10-29 15:56:04 +0400

    Update banner-rotator.js

--- kupolclimat_banner/js/banner-rotator.js
+++ kupolclimat_banner/js/banner-rotator.js
@@ -148,4 +148,5 @@ document.addEventListener('DOMContentLoaded', function() {
             bannerRotator.restartRotation();
         });
     }
+    console.log('Все скрипты инициализированы');
 });
