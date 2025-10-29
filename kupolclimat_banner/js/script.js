4814007f6f500e3c73bab89d8a74797f53e8dff8
L4NOTTE <145873589+L4NOTTE@users.noreply.github.com>
2025-10-29 15:48:45 +0400

    Add files via upload

--- kupolclimat_banner/js/script.js
+++ kupolclimat_banner/js/script.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Сайт загружен!');
-
     // Мобильное меню
     const menuToggle = document.querySelector('.menu-toggle');
     const nav = document.querySelector('.nav');
